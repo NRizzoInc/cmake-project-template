d2595e429dd81f0d04708359fee99a4f69ea625a
Nick Rizzo <nick.m.rizzo@medtronic.com>
2023-10-05 14:30:20 -0400

    [feat] Automate embedding build metadata within binary

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-#include <version.h>
-
 int main() {
-    std::cout << "GIT_SHA1=" << GIT_SHA1 << "\n";
-    std::cout << "GIT_BRANCH=" << GIT_BRANCH << "\n";
+    std::cout << "Hello World!\n";
     return EXIT_SUCCESS;
 }
--- src/version/public/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-extern const std::string GIT_SHA1;
-extern const std::string GIT_BRANCH;
-extern const std::string GIT_COMMIT_SUBJECT;
-extern const std::string GIT_DATE;
-extern const std::string GIT_DESCRIBE;
